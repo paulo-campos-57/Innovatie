df41b778de6ae15c60fbc822224116d6de882795
Virna Amaral <vfpa@cesar.school>
2023-05-11 14:48:37 -0300

    inserção das funções de menu

--- leimer/func_lib.c
+++ leimer/func_lib.c
@@ -13,3 +13,32 @@ void pausa() {
     getch();
 }
 
+void menu_residente() {
+    printf("Bem-Vindo, Residente!\n\n");
+    printf("[1] Marcar Presença\n");    
+    printf("[2] Consultar Feedbacks\n");
+    printf("[3] Dar Feedbacks\n");
+    printf("[4] Quadro de Avisos\n"); //apenas visualizar
+    printf("[5] Minhas Faltas\n");
+    printf("[0] Sair\n");
+    
+}
+
+void menu_preceptor() {
+    printf("Bem-Vindo, Preceptor!\n\n");
+    printf("[1] Validar Presenças\n");    
+    printf("[2] Consultar Feedbacks\n");
+    printf("[3] Dar Feedbacks\n");
+    printf("[4] Quadro de Avisos\n"); //opcao de visualizar e inserir avisos
+    printf("[0] Sair\n");
+}
+
+
+void menu_preceptor() {
+    printf("Bem-Vindo, Gestor!\n\n");
+    printf("[1] Gerar Código de Cadastro\n");    
+    printf("[2] Remover Usuário\n");
+    printf("[3] Consultar Feedbacks\n");
+    printf("[4] Quadro de Avisos\n"); //opcao de visualizar e inserir avisos
+    printf("[0] Sair\n");
+}
