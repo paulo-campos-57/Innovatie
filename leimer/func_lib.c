3c06a45c22dd93b1e05fc49a0ec67dd78cae5ee6
Virna Amaral <vfpa@cesar.school>
2023-05-11 14:29:42 -0300

    criação da lib de funções

---
+++ leimer/func_lib.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+//funcao para limpar tela
+void limpa_tela() {
+    system("CLS");
+}
+
+//funcao para pausar o programa.
+void pausa() {
+    printf("\n\nPressione qualquer tecla para continuar...\n");
+    getch();
+}
+
---
+++ leimer/func_lib.h
@@ -0,0 +1,8 @@
+#ifndef __FUNC_LIB__
+#define __FUNC_LIB__
+
+void limpa_tela();
+void pausa();
+
+
+#endif /* __FUNC_LIB__ */
