aacb04a8f66036130a9e33a9a54cbbfe7a145d85
Matheus Gomes de Andrade <117746778+MatheusGom@users.noreply.github.com>
2023-05-15 14:54:09 -0300

    Update leimer.c

--- leimer/leimer.c
+++ leimer/leimer.c
@@ -1,8 +1,42 @@
 #include<stdio.h>
+#include<stdlib.h>
+
+#define String_Max 100
 
 int main() {
-    printf("Hello World");
-    printf("ola mundinho");
-    printf("\n");
+    
+    char cadastro[String_Max], senha[String_Max];
+    int opcoes;
+    
+    printf("\tMenu\n");
+    printf("1 - Login\n");
+    printf("2 - Cadastro\n");
+    scanf("%d", &opcoes);
+
+    switch (opcoes) {
+    	
+        case 1:
+            printf("\tLogin\n");
+            printf("Username: ");
+            scanf("%s", cadastro);
+            printf("\n");
+            printf("Senha: ");
+            scanf("%s", senha);
+            break;
+        
+		case 2:
+            printf("\tCadastro\n");
+            printf("Username: ");
+            scanf("%s", cadastro);
+            printf("\n");
+            printf("Senha: ");
+            scanf("%s", senha);
+            break;
+        
+		default:
+            printf("Opcao invalida\n");
+            return 1;
+    }
+    
     return 0;
 }
